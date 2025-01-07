40b5893758851b1147edc8d4cf6f0b39f7dad395
Alexis Ferrere <alexferrere@outlook.fr>
2025-01-07 11:58:25 +0100

    problème controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,8 @@
 import { application } from "controllers/application"
 import DatepickerController from "./datepicker_controller"
 import MapController from "./map_controller"
+import AddressAutocompleteController from "./address_autocomplete_controller"
 
 application.register("datepicker", DatepickerController)
 application.register("map", MapController)
+application.register("address-autocomplete", AddressAutocompleteController)
